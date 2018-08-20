a9ddf8c42a18ccb919b8165c9b26353a4e432c49
George Alexandrov <gpalex@berkeley.edu>
2018-08-20 16:51:55 -0700

    removing lab 2 for now. will add labs incrementally as usual

--- lab2/sim/tests/adder_testbench.do
+++
@@ -1,3 +0,0 @@
-start adder_testbench 
-add wave adder_testbench/*
-run 10000ms
--- lab2/src/behavioral_adder.v
+++
@@ -1,7 +0,0 @@
-module behavioral_adder (
-    input [13:0] a,
-    input [13:0] b,
-    output [14:0] sum
-);
-    assign sum = a + b;
-endmodule
